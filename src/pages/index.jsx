ad3b13e4bd647b08a36467e2340625bfd02ecd92
PhiLongH <112935171+PhiLongH@users.noreply.github.com>
2023-10-10 15:41:17 +0700

    deleted:    src/pages/Courses.jsx 	modified:   src/pages/index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,4 @@
 export { default as Home } from './Home';
-export { default as Courses } from './Courses';
 export { default as ForgotPassword } from './ForgotPassword';
 export { default as Login } from './Login';
 export { default as Quiz } from './Quiz';
