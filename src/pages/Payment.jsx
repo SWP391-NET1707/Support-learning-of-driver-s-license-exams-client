e385fcc067785d6ebb40698df16edf3dcc117965
deadline-backup <timerush123@gmail.com>
2023-10-21 17:03:12 +0700

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/Payment.jsx

--- src/pages/Payment.jsx
+++ src/pages/Payment.jsx
@@ -1,33 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useLocation,useNavigate } from 'react-router-dom';
 
 const Payment = () => {
-  const [amount, setAmount] = useState(100000000); // Initial value, you can change it later
-  const [paymentUrl, setPaymentUrl] = useState(null);
+  const [amount, setAmount] = useState(10000000);
+  const [paymentUrl, setPaymentUrl] = useState('');
+
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const transactionStatus = searchParams.get('vnp_TransactionStatus');
+
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    if (transactionStatus === '00') {
+      navigate('/paysuccess');
+    } else {
+      navigate('/payfail');
+    }
+  };
+
+  useEffect(() => {
+    // Check if there are query parameters in the URL
+    if (location.search) {
+      handleRedirect();
+    }
+  }, [location.search, transactionStatus]);
 
   const handleDeposit = async () => {
+    const user = JSON.parse(sessionStorage.getItem("user"));
+
     try {
-      const response = await axios.post('https://drivingschoolapi20231005104822.azurewebsites.net/api/Transaction/deposit/vnpay', {
-        amount,
-        redirect: 'http://localhost:3000/payment',
-      });
+      const accessToken = user.accessToken;
+
+      const response = await axios.post(
+        'https://drivingschoolapi20231005104822.azurewebsites.net/api/Transaction/deposit/vnpay',
+        {
+          amount,
+          redirectUrl: 'http://localhost:3000/payment',
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${accessToken}`
+          }
+        }
+      );
+
+      console.log(amount, window.location.href);
 
       if (response.status === 200) {
         const { paymentUrl } = response.data;
-        setPaymentUrl(paymentUrl); // Set the paymentUrl in the component state
+        setPaymentUrl(paymentUrl);
 
-        // Automatically redirect to the paymentUrl
         if (paymentUrl) {
           window.location.href = paymentUrl;
         }
       } else {
-        // Handle an error response here
         console.error('Payment failed');
       }
     } catch (error) {
       console.error('An error occurred:', error);
     }
-  };
+  }
 
   return (
     <div>
